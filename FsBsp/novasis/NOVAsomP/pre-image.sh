4c821b7a80fd70041718cebc3f50e9599a25c482
Geninatti Paolo <tech@pagit.eu>
2022-06-04 11:12:56 +0200

    NovasomP: added kernel modules installation

--- FsBsp/novasis/NOVAsomP/pre-image.sh
+++ FsBsp/novasis/NOVAsomP/pre-image.sh
@@ -3,6 +3,8 @@
 echo "###### Customizing file system ######"
 echo "cp -r board/novasis/NOVAsomP/Init/* ${TARGET_DIR}"
 cp -r board/novasis/NOVAsomP/Init/* ${TARGET_DIR}
+echo "cp -r /Devel/NOVAsom_SDK/Deploy/lib ${TARGET_DIR}"
+cp -r /Devel/NOVAsom_SDK/Deploy/lib ${TARGET_DIR}
 chmod 777 ${TARGET_DIR}/bin/*
 chmod 777 ${TARGET_DIR}/etc/init.d/*
 sync
